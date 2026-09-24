dd774f936bbfb39c3d92bbc0af6a98f40486855b
Srivastava-samarth <srivastavasamarth94@gmail.com>
2026-09-25 03:03:25 +0530

    feat: added rate limiting and idempotency

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
-	handler "redis-lab/internal/controllers"
-	repository "redis-lab/internal/repositories"
-	service "redis-lab/internal/services"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -15,22 +15,18 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-func main() {
+type CreateURLRequest struct {
+	URL string `json:"url" binding:"required"`
+}
 
-	// --------------------------------------------------
-	// Environment
-	// --------------------------------------------------
+func main() {
+	ctx := context.Background()
 
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
 
-	ctx := context.Background()
-
-	// --------------------------------------------------
 	// PostgreSQL
-	// --------------------------------------------------
-
 	dsn := "postgres://" +
 		os.Getenv("DB_USER") + ":" +
 		os.Getenv("DB_PASSWORD") + "@" +
@@ -40,67 +36,197 @@ func main() {
 
 	db, err := pgxpool.New(ctx, dsn)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to create PostgreSQL pool:", err)
 	}
 	defer db.Close()
 
 	if err := db.Ping(ctx); err != nil {
-		log.Fatal("Database connection failed:", err)
+		log.Fatal("PostgreSQL connection failed:", err)
 	}
 
 	log.Println("Connected to PostgreSQL")
 
-	// --------------------------------------------------
 	// Redis
-	// --------------------------------------------------
-
 	redisClient := redis.NewClient(&redis.Options{
-		Addr:               "localhost:6380",
-		MaxRetries:         -1,
-		DialerRetries:      1,
-		DialTimeout:        100 * time.Millisecond,
-		DialerRetryTimeout: 0,
-		ReadTimeout:        100 * time.Millisecond,
-		WriteTimeout:       100 * time.Millisecond,
+		Addr: "localhost:6380",
 	})
+	defer redisClient.Close()
+
+	if err := redisClient.Ping(ctx).Err(); err != nil {
+		log.Fatal("Redis connection failed:", err)
+	}
 
 	log.Println("Connected to Redis")
 
-	// --------------------------------------------------
-	// Dependencies
-	// --------------------------------------------------
-
-	urlRepository := repository.NewURLRepository(db)
-	redisRepository := repository.NewRedisRepository(redisClient)
-
-	urlService := service.NewURLService(
-		urlRepository,
-		redisRepository,
-	)
-
-	urlHandler := handler.NewURLHandler(
-		urlService,
-	)
-
-	// --------------------------------------------------
-	// Routes
-	// --------------------------------------------------
-
 	router := gin.Default()
 
-	router.POST(
-		"/urls",
-		urlHandler.CreateURL(),
-	)
+	router.POST("/urls", func(c *gin.Context) {
+		var req CreateURLRequest
+		clientID := c.GetHeader("X-Client-ID")
+		if clientID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "X-Client-ID header is required",
+			})
+			return
+		}
 
-	router.GET(
-		"/urls/:shortCode",
-		urlHandler.GetURL(),
-	)
+		idempotencyKey := c.GetHeader("Idempotency-Key")
 
-	// --------------------------------------------------
-	// Server
-	// --------------------------------------------------
+		if idempotencyKey == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Idempotency-Key header is required",
+			})
+			return
+		}
+
+		idempotencyRedisKey := fmt.Sprintf(
+			"idempotency:%s:%s",
+			clientID,
+			idempotencyKey,
+		)
+
+		storedShortCode, errG := redisClient.Get(ctx, idempotencyRedisKey).Result()
+
+		if errG == nil {
+			if storedShortCode == "processing" {
+				c.JSON(http.StatusConflict, gin.H{
+					"error": "request is already being processed",
+				})
+				return
+			}
+
+			c.JSON(http.StatusOK, gin.H{
+				"short_code": storedShortCode,
+			})
+			return
+		}
+
+		if errG != redis.Nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "failed to check idempotency key",
+			})
+			return
+		}
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "url is required",
+			})
+			return
+		}
+
+		// idempotency doesn't exist so create a key
+		wasSet, errSI := redisClient.SetNX(ctx, idempotencyRedisKey, "processing", 24*time.Hour).Result()
+		if errSI != nil {
+			log.Fatal("error setting idempotecny key:", errSI)
+		}
+
+		if !wasSet {
+			c.JSON(http.StatusConflict, gin.H{
+				"error": "request is already being processed",
+			})
+			return
+		}
+
+		time.Sleep(10 * time.Second)
+
+		var shortCode string
+		counter := fmt.Sprintf("rate-limit:%s", clientID)
+		// setting counter to 0, if does not exist
+		_, errS := redisClient.SetNX(ctx, counter, 0, 60*time.Second).Result()
+		if errS != nil {
+			log.Fatal("error setting counter:", errS)
+		}
+
+		// increment the counter
+		id, errI := redisClient.Incr(ctx, counter).Result()
+		if errI != nil {
+			log.Fatal("error incrementing value:", errI)
+		}
+
+		if id > 5 {
+			_, err := redisClient.Del(ctx, idempotencyRedisKey).Result()
+			if err != nil {
+				log.Println("error deleting idempotency key:", err)
+			}
+
+			c.JSON(http.StatusTooManyRequests, gin.H{
+				"error": "rate limit exceeded",
+			})
+			return
+		}
+
+		counter2 := "url:id"
+		// increment the counter
+		id2, errII := redisClient.Incr(ctx, counter2).Result()
+		if errII != nil {
+			log.Fatal("error incrementing value:", errII)
+		}
+
+		shortCode = "test" + strconv.FormatInt(id2, 10)
+
+		err := db.QueryRow(
+			c,
+			"INSERT INTO urls (client_id, short_code, original_url) VALUES ($1, $2, $3) RETURNING short_code",
+			clientID,
+			shortCode,
+			req.URL,
+		).Scan(&shortCode)
+
+		if err != nil {
+			log.Println("Failed to create URL:", err)
+
+			_, err := redisClient.Del(ctx, idempotencyRedisKey).Result()
+			if err != nil {
+				log.Println("error deleting idempotency key:", err)
+			}
+
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "failed to create URL",
+			})
+			return
+		}
+
+		_, errIU := redisClient.Set(
+			ctx,
+			idempotencyRedisKey,
+			shortCode,
+			24*time.Hour,
+		).Result()
+
+		if errIU != nil {
+			log.Println("error updating the key:", errIU)
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{
+			"short_code": shortCode,
+			"url":        req.URL,
+		})
+	})
+
+	router.GET("/urls/:shortCode", func(c *gin.Context) {
+		shortCode := c.Param("shortCode")
+
+		var originalURL string
+
+		err := db.QueryRow(
+			c,
+			"SELECT original_url FROM urls WHERE short_code = $1",
+			shortCode,
+		).Scan(&originalURL)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "URL not found",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"url": originalURL,
+		})
+	})
 
 	log.Println("Server running on :8080")
 
---
+++ notes.md
@@ -0,0 +1,2 @@
+NX prevents concurrent first requests from both initializing the counter.
+NX prevents subsequent requests from resetting the expiration window.
